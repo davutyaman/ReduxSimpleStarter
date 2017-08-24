ecf0eb49c48b037304232d9d4c6595e8e97f576b
yamandavut <yamandavut@gmail.com>
2017-08-24 18:08:06 +0300

    lecture 26

---
+++ src/components/video_list_item.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const VideoListItem = ({ video }) => {
+    console.log(video);
+
+    //const video = props.video;
+    return (
+        <li className="list-group-item">
+            <div>
+                
+            </div>
+
+        </li>
+    );
+
+};
+
+export default VideoListItem;
